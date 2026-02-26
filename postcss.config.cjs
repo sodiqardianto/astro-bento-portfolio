b21336272176174f93cd55b81da0c72da83a0d5c
sodiqardianto <sodiqardianto@gmail.com>
2026-02-26 16:14:32 +0700

    fix: resolve PostCSS ESM/CJS interop & use pnpm on Netlify

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,9 @@
 // postcss.config.cjs
+const _unocss = require("@unocss/postcss");
+const UnoCSS = typeof _unocss === "function" ? _unocss : _unocss.default;
+
 module.exports = {
-  plugins: {
-    "@unocss/postcss": {},
-  },
+  plugins: [
+    UnoCSS(),
+  ],
 };
